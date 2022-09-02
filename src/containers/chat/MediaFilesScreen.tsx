a9632e4a163d3c8f874e86900d7618c8c5f37746
Soheng-CHHUM <soheng.chhum@gmail.com>
2022-09-02 13:23:55 +0700

    push all code

--- src/containers/chat/MediaFilesScreen.tsx
+++ src/containers/chat/MediaFilesScreen.tsx
@@ -25,8 +25,8 @@ import themeStyle from '../../styles/theme';
 
 const listheadertext = ['Media', 'File', 'Link']
 let lastDoc: any = 1;
-let perPage: any = 30;
-
+let perPage: any = 20;
+// let selectedHeader = 'media';
 const MediaFilesScreen = (props: any) => {
     const { userChat } = props.route.params
     const [headerIdx, setHeaderIdx] = useState(0)
@@ -34,14 +34,15 @@ const MediaFilesScreen = (props: any) => {
     const navigate: any = useNavigation();
     const { theme }: any = useContext(ThemeContext);
     const [hasScrolled, setHasScrolled] = useState(false);
-	const [isRefresh, setIsRefresh] = useState(false);
-	const [isMoreLoading, setIsMoreLoading] = useState(false);
+    const [isRefresh, setIsRefresh] = useState(false);
+    const [isMoreLoading, setIsMoreLoading] = useState(false);
     const [selectedHeader, setSeletedHeader] = useState('media')
 
     useEffect(() => {
         requestDocumentAPI(selectedHeader)
     }, []);
     const onSetHeaderItem = ({ item, index }: any) => {
+        setSeletedHeader(item.toLowerCase())
         requestDocumentAPI(item.toLowerCase())
         setHeaderIdx(index)
     }
@@ -80,11 +81,11 @@ const MediaFilesScreen = (props: any) => {
                 console.log('error')
             });
     }
-    const openLinkChat = (mess:any) =>{
+    const openLinkChat = (mess: any) => {
         let checkUrlLink = mess.message.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
         checkUrlLink != null ? Linking.openURL(checkUrlLink[0]) : null;
     }
-    
+
     const renderFooter: any = () => {
         if (!isMoreLoading) return true;
         return (
@@ -96,7 +97,7 @@ const MediaFilesScreen = (props: any) => {
         )
     };
 
-	const onRefresh = () => {
+    const onRefresh = () => {
         setIsRefresh(true)
         lastDoc = 1;
         requestDocumentAPI(selectedHeader);
@@ -104,43 +105,43 @@ const MediaFilesScreen = (props: any) => {
             setIsRefresh(false)
         }, 200);
     };
-	const _onScroll = () => {
+    const _onScroll = () => {
         if (!hasScrolled)
             setHasScrolled(true)
     };
     const getMore = async () => {
         if (!hasScrolled) return null;
         if (lastDoc > 0) {
-			setIsMoreLoading(true)
-			setTimeout(async () => {
-				GET(`chatroom/document/detail/${userChat.id}?type=${selectedHeader}&page=${lastDoc + 1}&per_page=${perPage}`)
-				.then(async (result) => {
-					if(result.status) {
-						lastDoc += 1;
-						let _data : any = documents;
-						if (result.status && result.data.data.length !== 0) {
-                            setDocuments([...result.data.data,_data])
-							_data.push(...result.data.data)
-						}
-						lastDoc = Math.ceil(_data.length / 20);
-						if (result.data.data !== undefined) {
-							if (result.data.total <= documents.length) {
-								lastDoc = 0;
-							}
-						}
-					}
-					setIsMoreLoading(false)
-				})
-				.catch(e => {
-					setIsMoreLoading(false)
-				});
-			}, 200);
+            setIsMoreLoading(true)
+            setTimeout(async () => {
+                GET(`chatroom/document/detail/${userChat.id}?type=${selectedHeader}&page=${lastDoc + 1}&per_page=${perPage}`)
+                    .then(async (result) => {
+                        if (result.status) {
+                            lastDoc += 1;
+                            let _data: any = documents;
+                            if (result.status && result.data.data.length !== 0) {
+                                setDocuments([...result.data.data, _data])
+                                _data.push(...result.data.data)
+                            }
+                            lastDoc = Math.ceil(_data.length / 20);
+                            if (result.data.data !== undefined) {
+                                if (result.data.total <= documents.length) {
+                                    lastDoc = 0;
+                                }
+                            }
+                        }
+                        setIsMoreLoading(false)
+                    })
+                    .catch(e => {
+                        setIsMoreLoading(false)
+                    });
+            }, 200);
         }
     };
 
     const _renderMedia = ({ item, index }: any) => {
         return (
-            <TouchableOpacity onPress={()=> item.type !='mp4' ? navigate.navigate('DisplayFullImg', { imgDisplay: item.file_url }) : onFullVideo(item.file_url)}  style={{ backgroundColor: '#E9E9E99D', height: 140, width: deviceWidth / 3.4, margin: 3, borderRadius: 5, borderColor: borderDivider, borderWidth: 0.5 }}>
+            <TouchableOpacity onPress={() => item.type != 'mp4' ? navigate.navigate('DisplayFullImg', { imgDisplay: item.file_url }) : onFullVideo(item.file_url)} style={{ backgroundColor: '#E9E9E99D', height: 140, width: deviceWidth / 3.4, margin: 3, borderRadius: 5, borderColor: borderDivider, borderWidth: 0.5 }}>
                 {item.type == 'mp4' ?
                     <View>
                         <Video
@@ -159,26 +160,26 @@ const MediaFilesScreen = (props: any) => {
                         </View>
 
                     </View>
-                : <Image source={{ uri: item.file_url }} style={{ width: '100%', height: '100%', borderRadius: 5 }} />}
+                    : <Image source={{ uri: item.file_url }} style={{ width: '100%', height: '100%', borderRadius: 5 }} />}
             </TouchableOpacity>
         )
     }
     const _renderFileandLink = ({ item, index }: any) => {
-        if(item.type =='mp3') return null;
+        if (item.type == 'mp3') return null;
         return (
             <TouchableOpacity onPress={() => item.type == 'url' ? openLinkChat(item) : _onOpenFile(item)} style={{ backgroundColor: theme == 'dark' ? primaryDark : '#F0F0F2', padding: main_padding, paddingBottom: 5, marginTop: 7, borderRadius: 10 }}>
                 <HStack alignItems='center'>
-                    {item.type == 'url' ? 
-                        <Feather name='link' size={25} color={textSecondColor}/>
-                    :
-                    <FontAwesome
-                        name={
-                            item.type == 'pdf' ? "file-pdf-o" : item.type == 'xls' || item.type == 'xlsx' ? 'file-excel-o'
-                                : item.type == 'ppt' || item.type == 'pptx' || item.type == 'csv' ? 'file-powerpoint-o'
-                                    : item.type == 'doc' || item.type == 'docx' ? 'file-word-o' : item.type == 'zip' ? 'file-zip-o' : 'file-text-o'
-                        } size={25} color={textSecondColor} />
+                    {item.type == 'url' ?
+                        <Feather name='link' size={25} color={textSecondColor} />
+                        :
+                        <FontAwesome
+                            name={
+                                item.type == 'pdf' ? "file-pdf-o" : item.type == 'xls' || item.type == 'xlsx' ? 'file-excel-o'
+                                    : item.type == 'ppt' || item.type == 'pptx' || item.type == 'csv' ? 'file-powerpoint-o'
+                                        : item.type == 'doc' || item.type == 'docx' ? 'file-word-o' : item.type == 'zip' ? 'file-zip-o' : 'file-text-o'
+                            } size={25} color={textSecondColor} />
                     }
-                    <Text style={{ fontSize: 13, fontFamily: 'Montserrat-Regular', marginLeft: main_padding - 5, textDecorationLine: item.type =='url' ? 'underline' : 'none', color: item.type == 'url' ? baseColor : theme == 'dark' ? '#D1D1D1' : textColor }}>{item.message + '.' + item.type}</Text>
+                    <Text style={{ fontSize: 13, fontFamily: 'Montserrat-Regular', marginLeft: main_padding - 5, textDecorationLine: item.type == 'url' ? 'underline' : 'none', color: item.type == 'url' ? baseColor : theme == 'dark' ? '#D1D1D1' : textColor }}>{item.message + '.' + item.type}</Text>
                 </HStack>
                 <Text style={{ fontFamily: 'Montserrat-Regular', textAlign: 'right', color: textSecondColor, fontSize: 10, marginTop: 5 }}>{moment(item.created_at).format('MMMM DD, YYYY  HH:mm')}</Text>
             </TouchableOpacity>
@@ -206,11 +207,26 @@ const MediaFilesScreen = (props: any) => {
                 key='media'
                 listKey={makeid()}
                 showsVerticalScrollIndicator={false}
-                scrollEnabled={true}
+                // inverted
                 data={documents}
                 renderItem={_renderMedia}
                 numColumns={3}
                 keyExtractor={(_, index) => index.toString()}
+                refreshControl={
+                    <RefreshControl refreshing={isRefresh} onRefresh={onRefresh} colors={[themeStyle[theme].textColor]} tintColor={themeStyle[theme].textColor} />
+                }
+                ListFooterComponent={
+                    <>
+                        {isMoreLoading && lastDoc !== 0 && renderFooter()}
+                        <Footer />
+                    </>
+                }
+                onTouchMove={_onScroll}
+                onEndReached={() => {
+                    if (!isMoreLoading && documents.length > 8) {
+                        getMore();
+                    }
+                }}
             />
         )
     }
@@ -233,12 +249,12 @@ const MediaFilesScreen = (props: any) => {
                             key='file'
                             listKey={makeid()}
                             showsVerticalScrollIndicator={false}
-                            scrollEnabled={true}
                             data={documents}
+                            // inverted
                             renderItem={_renderFileandLink}
                             keyExtractor={(_, index) => index.toString()}
                             refreshControl={
-                                <RefreshControl refreshing={isRefresh} onRefresh={onRefresh} colors={[themeStyle[theme].textColor]}  tintColor={themeStyle[theme].textColor} />
+                                <RefreshControl refreshing={isRefresh} onRefresh={onRefresh} colors={[themeStyle[theme].textColor]} tintColor={themeStyle[theme].textColor} />
                             }
                             ListFooterComponent={
                                 <>
@@ -248,7 +264,7 @@ const MediaFilesScreen = (props: any) => {
                             }
                             onTouchMove={_onScroll}
                             onEndReached={() => {
-                                if (!isMoreLoading &&  documents.length > 8) {
+                                if (!isMoreLoading && documents.length > 8) {
                                     getMore();
                                 }
                             }}
